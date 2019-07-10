b2c16cf9c751fab6084724b647ca8c899ff583b4
priyarora4 <52748057+priyarora4@users.noreply.github.com>
2019-07-10 09:21:35 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header'
+import uuid from 'uuid'
+
+class App extends Component{
+      state = {
+todos: [
+  
+]
+
+      }
+
+markComplete = (id) => {
+
+  this.setState( {todos: this.state.todos.map(todo =>{
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+  }
+  )} );
+}
+
+delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+}
+
+
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(), 
+    title: title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] });
+}
+
+  render() {
+
+    return (
+    <div className="App">
+        <div className="container"> 
+            <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete = {this.markComplete}
+          delTodo={this.delTodo} />
+        
+        </div>
+    </div>
+  );
+}
+}
+export default App;
